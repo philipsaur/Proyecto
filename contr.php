91d4690acdcf28a819eed6083d732febcc86243d
JulianG333 <132769312+JulianG333@users.noreply.github.com>
2023-06-16 18:03:28 -0500

    Update contr.php

--- contr.php
+++ contr.php
@@ -9,7 +9,7 @@ $Cantidad_bene = $_POST["CB"];
 $Destino = $_POST["DST"];
 $Nombre_conductor = $_POST["NomC"];
 
-$sql = "INSERT INTO persona (Fecha_viajes, Id_conductor, Id_tranporte, Cantidad_ben, Destino, Nombre_cond) VALUES ('$Fecha_viajes', '$Id_conductor', '$Id_transporte', '$Cantidad_bene', '$Destino', '$Nombre_conductor');";
+$sql = "INSERT INTO transporte (Fecha_viajes, Id_conductor, Id_tranporte, Cantidad_ben, Destino, Nombre_cond) VALUES ('$Fecha_viajes', '$Id_conductor', '$Id_transporte', '$Cantidad_bene', '$Destino', '$Nombre_conductor');";
 if($conn -> query($sql)){
     echo "Registro creado";
     include_once "Transporte.html";
