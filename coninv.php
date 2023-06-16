d1bb31b8fee065d0ca845a49b9ab89e9b94d6cf4
JulianG333 <132769312+JulianG333@users.noreply.github.com>
2023-06-16 17:46:11 -0500

    Create coninv.php

---
+++ coninv.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once "registrar.php";
+
+$Id_inventario = $_POST["IDI"];
+$Fecha_registro = $_POST["FD"]; 
+$Cantidad = $_POST["Cantidad"];
+$Id_donacion = $_POST["IDD"];
+$Archivados = $_POST["Archivados"];
+
+$sql = "INSERT INTO persona (Id_inventario, Fecha_registro, Cantidad, Id_donacion, Archivados) VALUES ('$Id_inventario', '$Fecha_registro', '$Cantidad', '$Id_donacion', '$Archivados');";
+if($conn -> query($sql)){
+    echo "Registro creado";
+    include_once "formulario.html";
+}
+else{
+    echo "Error, no se registró";
+}
+
+
+
+?>
