820750f8de0eb682bbcca64da0ce1a415d32994b
JulianG333 <132769312+JulianG333@users.noreply.github.com>
2023-06-16 18:06:38 -0500

    Update coninv.php

--- coninv.php
+++ coninv.php
@@ -8,7 +8,7 @@ $Cantidad = $_POST["Cantidad"];
 $Id_donacion = $_POST["IDD"];
 $Archivados = $_POST["Archivados"];
 
-$sql = "INSERT INTO persona (Id_inventario, Fecha_registro, Cantidad, Id_donacion, Archivados) VALUES ('$Id_inventario', '$Fecha_registro', '$Cantidad', '$Id_donacion', '$Archivados');";
+$sql = "INSERT INTO inventarios (Id_inventario, Fecha_registro, Cantidad, Id_donacion, Archivados) VALUES ('$Id_inventario', '$Fecha_registro', '$Cantidad', '$Id_donacion', '$Archivados');";
 if($conn -> query($sql)){
     echo "Registro creado";
     include_once "formulario.html";
