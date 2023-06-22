1a1313afe2cc287dc488cdb341d7c836ac4c57de
JulianG333 <132769312+JulianG333@users.noreply.github.com>
2023-06-22 18:10:04 -0500

    Update coninv.php

--- coninv.php
+++ coninv.php
@@ -7,11 +7,12 @@ $Fecha_registro = $_POST["FD"];
 $Cantidad = $_POST["Cantidad"];
 $Id_donacion = $_POST["IDD"];
 $Archivados = $_POST["Archivados"];
+$Estado = $_POST["EM"];
 
-$sql = "INSERT INTO inventarios (Id_inventario, Fecha_registro, Cantidad, Id_donacion, Archivados) VALUES ('$Id_inventario', '$Fecha_registro', '$Cantidad', '$Id_donacion', '$Archivados');";
+$sql = "INSERT INTO inventarios (Id_inventario, Fecha_registro, Cantidad, Id_donacion, Archivado, Estado_material) VALUES ('$Id_inventario', '$Fecha_registro', '$Cantidad', '$Id_donacion', '$Archivados', '$Estado');";
 if($conn -> query($sql)){
     echo "Registro creado";
-    include_once "formulario.html";
+    include_once "Registrarproducto.html";
 }
 else{
     echo "Error, no se registró";
